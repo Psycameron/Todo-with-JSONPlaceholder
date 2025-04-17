561f75b03386aa47e9dc77c4b396951047bb368f
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-04-17 15:55:34 +0300

    feat: Installed react query and added provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-
 export default function Home() {
   return (
-    <div >
-      <main>
-        <h1>Toto list with JSONPlaceholder</h1>
-      </main>
-      <footer >
-      </footer>
-    </div>
+    <main>
+      <h1>Toto list with JSONPlaceholder</h1>
+    </main>
   );
 }
