6f385c69ee47defc9bdfcb75d4375b1b60e17f37
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-04-17 19:22:41 +0300

    feat: Added todosApi and type

---
+++ src/api/todoApi.ts
@@ -0,0 +1,27 @@
+import { Todo } from "@/types/todo";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const getTodos = async (): Promise<Todo[]> => {
+  const res = await fetch(`${BASE_URL}/todos?_limit=10`);
+  if (!res.ok) throw new Error("Failed to fetch todos");
+  return res.json();
+};
+
+export const addTodo = async (title: string): Promise<Todo> => {
+  const res = await fetch(`${BASE_URL}/todos`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, completed: false }),
+  });
+  if (!res.ok) throw new Error("Failed to add todo");
+  return res.json();
+};
+
+export const deleteTodo = async (id: number): Promise<number> => {
+  const res = await fetch(`${BASE_URL}/todos/${id}`, {
+    method: "DELETE",
+  });
+  if (!res.ok) throw new Error("Failed to delete todo");
+  return id;
+};
