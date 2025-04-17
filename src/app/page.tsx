1758bdfa301ceb55bc43f69de1ac75600a39edb4
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-04-17 14:50:17 +0300

    Init project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div >
+      <main>
+        <h1>Toto list with JSONPlaceholder</h1>
+      </main>
+      <footer >
+      </footer>
+    </div>
+  );
+}
