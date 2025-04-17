bb843ffe8df017753625e949f894cfb91fc6497c
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-04-17 20:08:38 +0300

    styles: Added styles for delete button

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -7,7 +7,7 @@ const DeleteButton = ({ id, onDelete }: IDeleteButtonProps) => {
   return (
     <button
       onClick={() => onDelete(id)}
-      className="mt-3 text-sm text-red-500 hover:text-red-700 self-end"
+      className="cursor-pointer transition border-1 rounded-full px-2 mt-3 text-sm text-red-500 hover:text-red-700 self-end"
     >
       Delete
     </button>
